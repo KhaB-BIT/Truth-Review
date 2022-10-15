edf88b72a9ab8322f77abfa35eb0ab9576ccc9d8
Minh Kha Bui <buiminhkha24072001@gmail.com>
2022-10-15 19:35:19 +0700

    first commit

---
+++ src/components/HomePage/index.js
@@ -0,0 +1,12 @@
+import { Text } from "@chakra-ui/react";
+
+function Homepage() {
+  return (
+    <>
+      <Text>Home page</Text>
+      <Text>Header....</Text>
+    </>
+  );
+}
+
+export default Homepage;
---
+++ src/components/InfoPage/index.js
@@ -0,0 +1,11 @@
+import { Text } from "@chakra-ui/react";
+
+function InfoPage() {
+  return (
+    <>
+      <Text>Info page</Text>
+    </>
+  );
+}
+
+export default InfoPage;
